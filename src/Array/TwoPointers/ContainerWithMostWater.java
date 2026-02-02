2baf8a35cd7a023102fa7a3d56f920b15d66dd0a
akush1207 <aksh95605@gmail.com>
2026-02-02 19:38:18 +0530

    ContainerWithMostWater

--- src/Array/TwoPointers/ContainerWithMostWater.java
+++ src/Array/TwoPointers/ContainerWithMostWater.java
@@ -1,4 +1,34 @@
 package Array.TwoPointers;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ContainerWithMostWater {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the array elements separated by comma(,):");
+        String arr = sc.nextLine();
+        int[] height = Arrays.stream(arr.split(","))
+                .map(String::trim)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        System.out.println("Maximum water in container:" + maxArea(height));
+
+    }
+
+    public static int maxArea(int[] height) {
+        int maxWater = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            int minHeight = Math.min(height[left], height[right]);
+            maxWater = Math.max(minHeight * (right - left), maxWater);
+            if (minHeight == height[left]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxWater;
+    }
 }
